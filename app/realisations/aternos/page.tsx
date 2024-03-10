11844a800c5966a810db69551a2063e24acb039e
pc-matheus <matheuskopsguedes@gmail.com>
2024-03-10 22:46:37 +0100

    update: page aternos

--- app/realisations/aternos/page.tsx
+++ app/realisations/aternos/page.tsx
@@ -12,6 +12,7 @@ import {Section} from "@/components/Sections/Global/Section";
 const Aternos = () => {
   const lib = realisationsData.find((realisation) => realisation.link === '/aternos')
   return(
+    <>
       <Section full={true} className={`relative`}>
         <div className={`absolute top-10 left-10`}>
           <Button isIconOnly color="danger" aria-label="Back"
@@ -25,22 +26,22 @@ const Aternos = () => {
           <h1 className={`text-4xl font-semibold text-success`}>{lib?.title}</h1>
           <p>{lib?.description}</p>
         </Section>
-        
-        <Section full={true}>
-          <div className={`flex flex-col md:flex-row gap-10 md:mx-20 p-10 bg-success md:rounded-xl`}>
-            <div>
-              <p className={`mb-6`}>Mon clone:</p>
-              <Image alt={`image`} src={`/Aternos.png`} width={800} height={800} className={`rounded-2xl`}/>
-            </div>
-            <Divider className={`md:hidden font-semibold bg-primary-800`} />
-            <div>
-              <p className={`mb-6`}>Vrai site:</p>
-              <Image alt={`image`} src={`/RealAternos.png`} width={800} height={800} className={`rounded-2xl`}/>
-            </div>
-          </div>
-        </Section>
-      
       </Section>
+      
+      <Section className={`pb-0 md:pb-14`} full={true}>
+        <div className={`flex flex-col md:flex-row gap-10 md:mx-20 p-10 bg-success md:rounded-xl`}>
+          <div>
+            <p className={`mb-6`}>Mon clone:</p>
+            <Image alt={`image`} src={`/Aternos.png`} width={800} height={800} className={`rounded-2xl`}/>
+          </div>
+          <Divider className={`md:hidden font-semibold bg-primary-800`} />
+          <div>
+            <p className={`mb-6`}>Vrai site:</p>
+            <Image alt={`image`} src={`/RealAternos.png`} width={800} height={800} className={`rounded-2xl`}/>
+          </div>
+        </div>
+      </Section>
+    </>
   )
 }
 
