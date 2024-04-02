dc33fa4c80d47b1faf4d1921d368553e369460c8
Matheus PC DBCargo <matheuskopsguedes@gmail.com>
2024-04-02 17:07:04 +0200

    update avancement du bilan

--- app/bilan/page.tsx
+++ app/bilan/page.tsx
@@ -2,20 +2,31 @@
 import React from 'react';
 
 import {Hero} from "@/components/Sections/Global/Hero";
+import { Section } from '@/components/Sections/Global/Section';
+import {H2} from '@/components/Sections/Global/Title'
 
 const Bilan: React.FC = () => {
 	return (
-		<main className={`flex flex-col items-center min-h-[150vh]`}>
+		<>
 			<Hero>
 				<h1 className={`text-success text-4xl font-semibold`}>
 					Bilan
 				</h1>
 				<p>
-					{`  Voici un bilan de mes compétences et de mes projets réalisés. Vous pouvez consulter mes projets et mes compétences
-						dans les sections ci-dessous. N'hésitez pas à me contacter pour des collaborations ! 🚀🌟`}
+					{`  Voici un bilan de mon ressenti sur cette dernière année et de mes compétences acquises. 🚀🌟`}
 				</p>
 			</Hero>
-		</main>
+			<Section color='success' full={true}>
+				<H2 color='white'>
+					Ressenti année 2023-2024
+				</H2>
+				<p>
+					{`
+						
+					`}
+				</p>
+			</Section>
+		</>
 	)
 }
 export default Bilan;
