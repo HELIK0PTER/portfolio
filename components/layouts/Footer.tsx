860913eb0845a0a5922c3f0114c48c24d1d55c48
Matheus Kops Guedes <matheuskopsguedes@gmail.com>
2025-06-04 15:59:35 +0200

    Remove outdated comment in Footer

--- components/layouts/Footer.tsx
+++ components/layouts/Footer.tsx
@@ -18,7 +18,6 @@ export const Footer = () => {
         <Link href={`https://gitlab.com/HELIK0PTER`} target={'_blank'} className={`flex items-center justify-center gap-1`}> <FaGitlab /> GitLab</Link>
         <Link href={`https://www.linkedin.com/in/matheus-kops-guedes-4293b7213/`} target={'_blank'} className={`flex items-center justify-center gap-1`}> <FaLinkedin /> Linkedin</Link>
       </div>
-      {/* faire une page mentions légales*/}
       <Link href={`/mentionslegales`}>Mentions Légales</Link>
     </footer>
   );
